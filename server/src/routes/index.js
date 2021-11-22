c2220424e39f36e067cdb0b8dd0299e0cc744319
Matias Vallejos <zk-@live.com.ar>
2021-11-22 13:20:31 -0300

    Agrego el boilerplate junto a un gitignore
    En el boilerplate esta el archivo index y app para levantar el servidor y db para la carga y relacion de modelos.
    
    Ademas el gitignore para que estemos todos en la misma sintonia y evitar problemas en futuros merges.

---
+++ server/src/routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+// Main path
+router.get('/', (req, res) => {
+    res.send("index");
+})
+
+module.exports = router;
