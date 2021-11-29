5351e0a4faad043e3d0a8a3ea49df041a45c1520
Matias Vallejos <zk-@live.com.ar>
2021-11-29 14:56:54 -0300

    mercadoPago routes
    Rutas de mercadopago

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const usersRoute = require('./users.js')
 const articlesRoute = require('./articles.js')
+const mpRoute = require('./mercadopago.js')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -10,6 +11,7 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/users", usersRoute);
 router.use("/articles", articlesRoute);
+router.use("/mp", mpRoute);
 
 // Main path
 router.get('/', (req, res) => {
